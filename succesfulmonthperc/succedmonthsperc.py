93e1a0dfa8b4b8dad4136b563fc6d9bdef9f7a71
rafal <rafal.lisak1@gmail.com>
2019-06-21 00:26:49 +0200

    asd

--- succesfulmonthperc/succedmonthsperc.py
+++ succesfulmonthperc/succedmonthsperc.py
@@ -6,6 +6,7 @@ from matplotlib.pyplot import figure
 from matplotlib import colors as mcolors
 from datetime import datetime
 
+
 df = read_csv('../categories')
 all_p = len(df)
 df2 = df
@@ -44,7 +45,7 @@ df = df[df.state == 'successful']
 mng = plt.get_current_fig_manager()
 mng.full_screen_toggle()
 set_option('display.max_rows', 500)
-figure(figsize=(30, 18), dpi=200)
+
 start = df.launched
 start = [x.split(" ")[0] for x in start]
 years = []
@@ -57,10 +58,7 @@ df3 = DataFrame({"month":years,"state":df.state})
 
 
 categories = df3.groupby('month')['month'].count()
-red = Color("gold")
-colors = list(red.range_to(Color("blue"),len(categories)+10))
-colors = [x.get_hex() for x in colors if len(x.get_hex())==7]
-colors=colors[2:]
+
 
 val = categories2.values
 mon = categories.values
@@ -71,16 +69,20 @@ result = [int(round((mon[i]/val[i])*100)) for i in range(len(val))]
 result2 = [int(round((mon2[i]/val[i])*100)) for i in range(len(val))]
 
 a = [str(i) for i in range(1,13)]
-width = 0.35
-ax1 = plt.bar(a,result2,width*2, color="yellowgreen")
-ax = plt.bar(a,result,width, color="gold")
-plt.legend((ax1, ax), ('Failed', 'Successful'))
 
-plt.ylim(0, 100)
-plt.xlabel("Months",fontsize=25)
-plt.ylabel("Percent of project",fontsize=25)
-plt.figtext(.5,.9,'Failed and successful projects', fontsize=30, ha='center')
+pol = DataFrame({'successful':result,"failed":result2})
+pol.index +=1
+print(pol)
 
-plt.savefig('successmonthperc.png')
+plt.clf()
+
+ax =pol.plot(kind='bar', color=["gold","green"],title= 'Failed and successful projects per months',fontsize=20,ylim=(0,100))
+ax.set_xlabel("Months",fontsize=10)
+ax.set_ylabel("Percent of project",fontsize=10)
+ax.title.set_size(17)
+
+plt.savefig('successmonthperc.png',bbox_inches="tight", pad_inches=0.1,dpi=200)
+
+
 
 
